10e59430eb064ca8f8c7c9316ff5b7d79cba8f37
InWoong <dlsdnd122@naver.com>
2025-07-22 11:51:38 +0900

    home update commit

---
+++ Project/Home/home.js
@@ -0,0 +1,6 @@
+// const hamburger = document.getElementById('hamburger');
+// const sidebar = document.getElementById('sidebar');
+//
+// hamburgeer.addEventListener('click', () => {
+//   sidebar.classList.toggle('collapsed');
+// });
