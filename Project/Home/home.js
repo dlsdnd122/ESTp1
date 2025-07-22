c4789f6b0d50bfe4720e4ef2b1d6cf1a05af3636
InWoong <dlsdnd122@naver.com>
2025-07-22 16:40:17 +0900

    docs : add text's html,css commit

--- Project/Home/home.js
+++ Project/Home/home.js
@@ -1,6 +1,20 @@
-// const hamburger = document.getElementById('hamburger');
-// const sidebar = document.getElementById('sidebar');
-//
-// hamburgeer.addEventListener('click', () => {
-//   sidebar.classList.toggle('collapsed');
-// });
+// 햄버거 바 클릭 시 사이드바 바뀜
+  document.addEventListener('DOMContentLoaded', function () {
+  const menuBtn = document.querySelector('.menu-btn');
+  const sidebar = document.getElementById('sidebar');
+  const sidebar2 = document.querySelector('.sidebar2');
+
+  menuBtn.addEventListener('click', function () {
+  // toggle 사이드바 표시 상태
+  const sidebarVisible = getComputedStyle(sidebar).display !== 'none';
+
+  if (sidebarVisible) {
+  sidebar.style.display = 'none';
+  sidebar2.style.display = 'flex'; // flex로 설정해야 내부 버튼들이 세로로 정렬됨
+} else {
+  sidebar.style.display = 'block';
+  sidebar2.style.display = 'none';
+}
+});
+});
+
