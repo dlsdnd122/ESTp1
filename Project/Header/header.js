e1d7707272c19d225d792a47c55b357a6cf71744
InWoong <dlsdnd122@naver.com>
2025-07-23 17:20:36 +0900

    docs : header file commit

---
+++ Project/Header/header.js
@@ -0,0 +1,36 @@
+//
+// // 햄버거바 클릭 이벤트 (사이드바 변경, main margin값 변경)
+// document.addEventListener('DOMContentLoaded', function () {
+//   const menuBtn = document.querySelector('.menu-btn');
+//   const sidebar = document.getElementById('sidebar');
+//   const sidebar2 = document.querySelector('.sidebar2');
+//
+//   menuBtn.addEventListener('click', function () {
+//     // toggle 사이드바 표시 상태
+//     const sidebarVisible = getComputedStyle(sidebar).display !== 'none';
+//
+//     if (sidebarVisible) {
+//       sidebar.style.display = 'none';
+//       sidebar2.style.display = 'flex'; // flex로 설정해야 내부 버튼들이 세로로 정렬됨
+//     } else {
+//       sidebar.style.display = 'block';
+//       sidebar2.style.display = 'none';
+//     }
+//   });
+// });
+//
+//
+// // 프로필 드롭다운 이벤트
+// const profileImg = document.getElementById('profile-img');
+// const dropdown = document.getElementById('dropdownMenu');
+// profileImg.addEventListener("click", () => {
+//   dropdown.style.display = dropdown.style.display === "block" ? "none" : "block";
+// });
+// // 바깥 클릭 시 드롭다운 닫기
+// document.addEventListener("click", (event) => {
+//   if (!event.target.closest(".profile-wrapper")) {
+//     dropdown.style.display = "none";
+//   }
+// });
+// console.log("헤더 스크립트 실행됨");
+//
