7e11f77d8f842384faa36b79b0567807ac0b0f52
InWoong <dlsdnd122@naver.com>
2025-07-30 15:29:34 +0900

    feat: 썸네일 data 추가 commit

--- Project/Video/js/video_2.js
+++ Project/Video/js/video_2.js
@@ -21,7 +21,129 @@ const videoData = {
 @hyoja94 송형주
 
 비즈니스 문의 - elzksms222@naver.com`
-    }
+    },
+
+    "2": {
+        videoUrl: "https://www.youtube.com/embed/42dPcnz1RPw?si=2eOFT2s2TwH6AQvE",
+        title: "휴가 복귀 3시간 전",
+        producerImg: "../video_img/2-c.jpg",
+        producerName: "숏박스",
+        producerSubs: "구독자 346만명",
+        viewsAndData: "조회수 7,803,411회 2024. 12. 6.",
+        description:
+            `장소 협찬 : 정성순대 부천중동점`
+    },
+
+    "3": {
+        videoUrl: "https://www.youtube.com/embed/As-lHM6emmk?si=FOO5sKK2Xs_Gh0sG",
+        title: "(실제 거장이 만든) 예술적인 방탈출 게임",
+        producerImg: "../video_img/3-c.jpg",
+        producerName: "우주하마",
+        producerSubs: "구독자 212만명",
+        viewsAndData: "조회수 144,088회 2025. 7. 22.",
+        description:
+            `리빙 홈 Leaving Home
+우주하마 UZUHAMA`
+    },
+
+    "4": {
+        videoUrl: "https://www.youtube.com/embed/rNjxjnl0LXY?si=AGQZ0WjK6m0pTnMA",
+        title: "나처럼 생긴 사람이 많은 나라 여행기 [뉴욕1]",
+        producerImg: "../video_img/4-c.jpg",
+        producerName: "레오티비 LEOTV",
+        producerSubs: "구독자 75만명",
+        viewsAndData: "조회수 337,675회 2025. 7. 21.",
+        description:
+            `인스타그램 leo.ranta
+네이버 카페 https://cafe.naver.com/leotv94</a>`
+    },
+
+    "5": {
+        videoUrl: "https://www.youtube.com/embed/xrULJRwYgOE?si=x5R-XFw8di6a4PN-",
+        title: "진실을 파헤치겠습니다.",
+        producerImg: "../video_img/5-c.jpg",
+        producerName: "랄로",
+        producerSubs: "구독자 130만명",
+        viewsAndData: "조회수 1,028,087회 2025. 7. 18.",
+        description:
+            `랄로 풀코스 /@랄롱
+치지직 생방송 https://naver.me/FWf4gAUW
+각종 비즈니스 aba060847@gmail.com`
+    },
+
+    "6": {
+        videoUrl: "https://www.youtube.com/embed/8gPaUgSO8zQ?si=l9EQX_g7HC3g-L_c",
+        title: "디저트 1타강사 아조씨의 최애 맛집 (ft.아조씨 시사 교양)",
+        producerImg: "../video_img/6-c.jpg",
+        producerName: "추성훈 ChooSungHoon",
+        producerSubs: "구독자 188만명",
+        viewsAndData: "조회수 812,524회 2025. 7. 17.",
+        description:
+            `안녕하심까 추성훈입니다.
+요로분 데쟈트 조아하시죠?
+제가 진촤 마싰눈 데쟈트 집을 소개하고 시포소 한번 갔다 와 봤숨니다.
+사실 여기눈 진촤 공개 안 하고 싶었눈데..
+그래도 구짜 요로분이 좋아하니까 공개함니돠
+가치 마싰눈 거 많이 먹으러 다니면 좋겠숨니다. ^_^
+
+[식당정보]
+[마츠바야 사료]`
+    },
+
+    "7": {
+        videoUrl: "https://www.youtube.com/embed/-8JTwRH23VY?si=ISyt2NEX8IfSE7Gk",
+        title: "이제 대세는 Claude Code!? 찐 꿀팁 대방출🔥",
+        producerImg: "../video_img/7-c.jpg",
+        producerName: "노마드 코더 Nomad Coders",
+        producerSubs: "구독자 50만명",
+        viewsAndData: "조회수 48,722회 2025. 7. 20.",
+        description:
+            `자비없는 성능....!
+고수처럼 Claude Code 설정하기!
+.
+💥 2025 신규강의! 에약판매 시작했어요 - AI Agents 마스터클래스
+새로운 AI 패러다임의 핵심은 바로 '에이전트'입니다.
+현 시점 최신 강의와 함께 AI 시대 변화의 흐름을 주도하세요
+5개 프레임워크 × 10개 실전 위주 프로젝트로 AI Agents의 모든 것을 배웁니다.
+CrewAI, Autogen, OpenAI Agents SDK, ADK, LangGraph ⚡️
+슈퍼 AI 개발자 되기 👉🏻 http://bit.ly/3J3DV2E
+-
+0:00~0:49 — 인트로
+0:50~1:18 — Claude Code 셋팅
+1:19~2:01 — Claude Code 작동원리
+2:02~4:29 — 고수처럼 Claude Code 설정하기
+4:30~5:42 — Claude Code 의 3가지 모드
+5:43~8:14 — Claude Code 의 슬래시 명령어
+8:15~9:56 — 최애 워크플로우 레시피`
+    },
+
+    "8": {
+        videoUrl: "https://www.youtube.com/embed/KsX0iJGME4I?si=LEKlHeVCkJPmzFtt",
+        title: "wait...you like lofi hip-hop?",
+        producerImg: "../video_img/8-c.jpg",
+        producerName: "the bootleg boy",
+        producerSubs: "구독자 424만명",
+        viewsAndData: "조회수 12,760회 2025. 7. 16.",
+        description:
+            `Listen on Spotify - https://spoti.fi/43uhUi9
+
+Full album titled 'Listen to my Story' by 97SPECIAL
+https://open.spotify.com/album/4sxzR2...
+
+Tracklist:
+our story
+again
+together with the moonlight
+she's gone
+because...
+decision
+sleep all day
+For U
+she's special
+lost love
+with you
+between the bar`
+    },
 }
 
 // ==================================
